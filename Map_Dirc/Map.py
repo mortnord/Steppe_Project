4ba576c4da1b352c9a8d5226921e7efcc653f406
mortnord <mortnord@gmail.com>
2022-08-02 16:26:37 +0200

    test

--- Map_Dirc/Map.py
+++ Map_Dirc/Map.py
@@ -1,67 +1,50 @@
 import random
 import math
 
+import arcade
+
 from Generation import Initial_Generation
 from Map_Dirc import Map_Region, Connection, Landscape
 from Static_Data import Static_Data
 
 
-def find_connections(region):
-    connection_1 = 10000
-    connection_2 = 10000
-    connection_3 = 10000
-    target_x_1 = 0
-    target_y_1 = 0
-    nr_target_1 = 0
-    target_x_2 = 0
-    target_y_2 = 0
-    nr_target_2 = 0
-    target_x_3 = 0
-    target_y_3 = 0
-    nr_target_3 = 0
+def make_connection(x_position, y_position, target_x, target_y, nr_target, region, distance):
+    region.connections.append(
+        Connection.Map_Connection(x_position, y_position, target_x, target_y, distance, nr_target))
+
+
+def find_connections():
     for x in range(len(Static_Data.get_map_with_regions())):
-        distance = (math.dist([region.x_position, region.y_position], [Static_Data.get_map_with_regions()[x].x_position,
-                                                                       Static_Data.get_map_with_regions()[
-                                                                           x].y_position]))
-        if distance == 0.0:
-            pass
-        elif distance < connection_1:
-            connection_1 = distance
-            target_x_1 = Static_Data.get_map_with_regions()[x].x_position
-            target_y_1 = Static_Data.get_map_with_regions()[x].y_position
-            nr_target_1 = x
-        elif distance < connection_2:
-            connection_2 = distance
-            target_x_2 = Static_Data.get_map_with_regions()[x].x_position
-            target_y_2 = Static_Data.get_map_with_regions()[x].y_position
-            nr_target_2 = x
-        elif distance < connection_3:
-            connection_3 = distance
-            target_x_3 = Static_Data.get_map_with_regions()[x].x_position
-            target_y_3 = Static_Data.get_map_with_regions()[x].y_position
-            nr_target_3 = x
-    region.connections.append(
-        Connection.Map_Connection(region.x_position, region.y_position, target_x_1, target_y_1, connection_1, nr_target_1))
-
-    region.connections.append(
-        Connection.Map_Connection(region.x_position, region.y_position, target_x_2, target_y_2, connection_2, nr_target_2))
-
-    region.connections.append(
-        Connection.Map_Connection(region.x_position, region.y_position, target_x_3, target_y_3, connection_3, nr_target_3))
+        for y in range(len(Static_Data.get_map_with_regions())):
+            distance = arcade.get_distance(Static_Data.get_map_with_regions()[x].x_position,
+                                           Static_Data.get_map_with_regions()[x].y_position,
+                                           Static_Data.get_map_with_regions()[y].x_position,
+                                           Static_Data.get_map_with_regions()[y].y_position)
+            make_connection(Static_Data.get_map_with_regions()[x].x_position,
+                            Static_Data.get_map_with_regions()[x].y_position,
+                            Static_Data.get_map_with_regions()[y].x_position,
+                            Static_Data.get_map_with_regions()[y].y_position,
+                            Static_Data.get_map_with_regions()[x].landscape.Landscapes_ID,
+                            Static_Data.get_map_with_regions()[x],
+                            distance)
+        Static_Data.get_map_with_regions()[x].connections.sort(key=lambda z : z.distance)
+        while len(Static_Data.get_map_with_regions()[x].connections) > 4:
+            Static_Data.get_map_with_regions()[x].connections.pop()
 
 
 def map_generation():
+    zoom_multiplier = 40
     list_of_regions = []
-    list_of_regions.append(Map_Region.Region(-4, 0, Landscape.City()))
+    list_of_regions.append(Map_Region.Region(-7 * zoom_multiplier, 0 * zoom_multiplier, Landscape.City()))
 
-    for x in range(-4,4):
-        for y in range(-6,6):
+    for x in range(-6, 6):
+        for y in range(-4, 4):
             rand_nr = random.randint(1, 5)
             if rand_nr == 1:
-                list_of_regions.append(Map_Region.Region(x + 1, y,Initial_Generation.next_map_generation()))
+                list_of_regions.append(Map_Region.Region((x + 1) * zoom_multiplier, y * zoom_multiplier,
+                                                         Initial_Generation.next_map_generation()))
 
-    list_of_regions.append(Map_Region.Region(5, 0,Landscape.City()))
+    list_of_regions.append(Map_Region.Region(7 * zoom_multiplier, 0 * zoom_multiplier, Landscape.City()))
 
     Static_Data.set_map_with_regions(list_of_regions)
-    for x in range(len(Static_Data.get_map_with_regions())):
-        find_connections(Static_Data.get_map_with_regions()[x])
+    find_connections()
--- Map_Dirc/Map_Region.py
+++ Map_Dirc/Map_Region.py
@@ -1,4 +1,7 @@
+import random
+
 from Generation import Initial_Generation
+from Static_Data import Static_Data
 
 
 class Region:
@@ -7,4 +10,13 @@ class Region:
         self.x_position = x_pos
         self.y_position = y_pos
         self.connections = []
+        self.nr_region = landscape.Landscapes_ID
+        self.scramble()
         print(x_pos, y_pos)
+
+    def scramble(self):
+        self.x_position += random.randint(-10, 10)
+        self.y_position += random.randint(-10, 10)
+        for x in range(len(self.connections)):
+            self.connections[x].own_x = self.x_position
+            self.connections[x].own_y = self.y_position
