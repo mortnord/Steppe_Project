0cf31578cd85ef08309732ff4ff508bc5e5d0c2c
mortnord <mortnord@gmail.com>
2022-08-07 11:58:17 +0200

    Combat View V0

--- Events/Combat_Event.py
+++ Events/Combat_Event.py
@@ -6,5 +6,10 @@ def combat_event(): #Kamp event
     enemies = []
     amount_enemies = random.randint(2, 3) #Generer mengden enemies
     for x in range(amount_enemies):
-        enemies.append(Goblins.Goblin()) #Lag nye gobling objekter, som er faktiske enemeies.
+        type_enemy = random.randint(1,6)
+        if type_enemy == 6:
+            pass
+            # enemies.append(#INSERT ORC KODE HER#)
+        else:
+            enemies.append(Goblins.Goblin()) #Lag nye gobling objekter, som er faktiske enemeies.
     return enemies
