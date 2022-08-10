6825883df269fb89bfb7ae7a9eca5ea763fedb12
mortnord <mortnord@gmail.com>
2022-08-10 15:49:11 +0200

    Ide for fjerning av primitive tekst implementert.

--- Enumerators.py
+++ Enumerators.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-#Enumeratorer er en egen definert variabel, som man kan bruke for å sammenligne og sjekke ting, f.eks at TypeOfSheep = Ram osv.
+# Enumeratorer er en egen definert variabel, som man kan bruke for å sammenligne og sjekke ting, f.eks at TypeOfSheep = Ram osv.
 import Enemies.Goblins
 
 
@@ -10,6 +10,7 @@ class TypeOfSheep(Enum):
     Male_Lamb = "Male lamb"
     Female_Lamb = "Female Lamb"
 
+
 class Landscapes(Enum):
     Steppes = "Steppes"
     Wooded = "Wooded"
@@ -17,10 +18,12 @@ class Landscapes(Enum):
     Rocky = "Rocky"
     City = "City"
 
+
 class TypeOfBuilding(Enum):
     Silo = "Silo"
     Wagon = "Wagon"
     Cheesery = "Cheesery"
+
 
 class TypeOfCard(Enum):
     Attack = "Attack"
@@ -30,15 +33,18 @@ class TypeOfCard(Enum):
     Heavy_Attack = "Heavy Attack"
     Healing = "Healing"
 
+
 class TypeOfPlannedAttack(Enum):
     Attack = "Attack"
     Defend = "Defend"
     Healing = "Healing"
 
+
 class Sprites_of_planned_attack(Enum):
     Attack = "Assets/sword.png"
     Defend = "Assets/shield.png"
     Healing = "Assets/healing.png"
+
 
 class Sprites(Enum):
     Card = "Assets/card.png"
@@ -51,6 +57,20 @@ class Sprites(Enum):
     Energy = "Assets/energy.png"
     End_Turn = "Assets/End_Turn.png"
 
+
 class Monsters(Enum):
     Goblin = "Goblin"
     Slime = "Slime"
+
+
+class Numbers(Enum):
+    Zero = "Assets/Numbers/Number-0-0.png"
+    One = "Assets/Numbers/Number-1-0.png"
+    Two = "Assets/Numbers/Number-2-0.png"
+    Three = "Assets/Numbers/Number-3-0.png"
+    Four = "Assets/Numbers/Number-4-0.png"
+    Five = "Assets/Numbers/Number-5-0.png"
+    Six = "Assets/Numbers/Number-6-0.png"
+    Seven = "Assets/Numbers/Number-7-0.png"
+    Eight = "Assets/Numbers/Number-8-0.png"
+    Nine = "Assets/Numbers/Number-9-0.png"
--- GUI_Dirc/Combat_View.py
+++ GUI_Dirc/Combat_View.py
@@ -6,10 +6,6 @@ from Commands_Dirc import Combat, Deck_management
 from GUI_Dirc import GUI
 from Static_Data import Static_Data
 
-# lag dette på nytt, der en kamp situasjon skifter til denne scenen, som lager fiender og alt relatert til kamp blir gjort her.
-# lag fiender
-# håndter fiender
-#
 from Static_Data_Bools import Static_Data_Bools
 
 
@@ -55,6 +51,8 @@ class Combat_View(arcade.View):
         self.active_dwarf = Static_Data.get_list_of_people()[0]
         self.end_turn.append(arcade.Sprite(Enumerators.Sprites.End_Turn.value, 0.10))
         self.setup_done = True
+        #Kan bruke dette til å generate nummere for alle objekter, som er sprites og ikke en primitive
+        self.test_list = self.make_SpriteList_from_numbers(60534, 100, 150)
 
     def change_active_dwarf(self):
         for x in range(len(Static_Data.get_list_of_people())):
@@ -91,8 +89,6 @@ class Combat_View(arcade.View):
         self.sprites_list_dwarves.clear()
         self.sprites_list_dwarves_energy.clear()
         self.active_dwarf_pointer.clear()
-        # Hvordan referere til active dwarf
-        # self.sprites_list_dwarves[Static_Data.get_list_of_people().index(self.active_dwarf)].center_y = 30
 
         self.active_dwarf_pointer.append(arcade.Sprite(Enumerators.Sprites.Active_Dwarf_Pointer.value, 0.10))
         for x in range(len(Static_Data.get_list_of_people())):
@@ -177,6 +173,7 @@ class Combat_View(arcade.View):
 
             self.active_dwarf_pointer.draw()
             self.end_turn.draw()
+            self.test_list.draw()
             self.draw_text()
 
     def draw_text(self):
@@ -312,3 +309,34 @@ class Combat_View(arcade.View):
         # We are no longer holding cards
         self.held_card = []
         self.held_card_indicator = []
+
+    def make_SpriteList_from_numbers(self, number_inn, x_position_inn, y_position_inn):
+        sprite_list_temporary = arcade.SpriteList()
+        SIZE_CONSTANT = 0.30
+        string_number = str(number_inn)
+        for x in range(len(string_number)):
+            if string_number[x] == "0":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Zero.value, SIZE_CONSTANT))
+            elif string_number[x] == "1":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.One.value, SIZE_CONSTANT))
+            elif string_number[x] == "2":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Two.value, SIZE_CONSTANT))
+            elif string_number[x] == "3":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Three.value, SIZE_CONSTANT))
+            elif string_number[x] == "4":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Four.value, SIZE_CONSTANT))
+            elif string_number[x] == "5":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Five.value, SIZE_CONSTANT))
+            elif string_number[x] == "6":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Six.value, SIZE_CONSTANT))
+            elif string_number[x] == "7":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Seven.value, SIZE_CONSTANT))
+            elif string_number[x] == "8":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Eight.value, SIZE_CONSTANT))
+            elif string_number[x] == "9":
+                sprite_list_temporary.append(arcade.Sprite(Enumerators.Numbers.Nine.value, SIZE_CONSTANT))
+        for x in range(len(sprite_list_temporary)):
+            sprite_list_temporary[x].center_x = x_position_inn + (x*24)
+            sprite_list_temporary[x].center_y = y_position_inn
+        return sprite_list_temporary
+
